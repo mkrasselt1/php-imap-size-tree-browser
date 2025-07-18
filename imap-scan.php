8131592433ab21d3936a458c404ee5ce7bda5e53
Michael Krasselt <michael@michaelkrasselt.de>
2025-07-18 12:12:24 +0200

    Ändere die SSL-Überprüfung im PHP-Skript von 'true' zu 'on' und entferne die nicht benötigte Umwandlung im JavaScript-Formular.

--- imap-scan.php
+++ imap-scan.php
@@ -9,7 +9,7 @@ $server = $_POST['server'] ?? '';
 $port   = $_POST['port'] ?? '993';
 $user   = $_POST['user'] ?? '';
 $pass   = $_POST['pass'] ?? '';
-$ssl    = ($_POST['ssl'] ?? 'false') === 'true';
+$ssl    = ($_POST['ssl'] ?? 'false') === 'on';
 
 if (!$server || !$user || !$pass) {
     http_response_code(400);
